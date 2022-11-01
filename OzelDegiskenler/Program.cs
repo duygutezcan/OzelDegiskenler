92c6a6940e6f0d548645ef695085ad847e713485
SEM <SEM@DESKTOP-4GKM6HK>
2022-11-01 10:08:28 +0300

    Add project files.

---
+++ OzelDegiskenler/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+// See https://aka.ms/new-console-template for more information
+ConsoleColor onRenk = ConsoleColor.Green;
+ConsoleColor arkaRenk = ConsoleColor.Red;
+Console.ForegroundColor = onRenk;
+Console.BackgroundColor = arkaRenk;
+
+int x = 100;
+x = x + 200;
+decimal y = 100.25M;
+float z = 100.25f;
+bool deger = true;
+deger = !deger;
+string ad = "Duygu";
+Console.WriteLine("int" + x);
+Console.WriteLine("decimal" + y);
+Console.WriteLine("Mantıksal" + deger);
+Console.WriteLine("Katar" + ad);
+
+
